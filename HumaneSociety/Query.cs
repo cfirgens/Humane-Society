86f79c6c9eaddbe4b77f2743156e9e8542864605
cfirgens <firgenscj@gmail.com>
2019-07-31 13:57:26 -0500

    added in query for pending adoptions

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -225,7 +225,8 @@ namespace HumaneSociety
 
         internal static IQueryable<Adoption> GetPendingAdoptions()
         {
-            throw new NotImplementedException();
+            IQueryable<Adoption> pendingAdoptions = db.Adoptions.Where(a => a.ApprovalStatus == "Pending");
+            return pendingAdoptions;
         }
 
         internal static void UpdateAdoption(bool isAdopted, Adoption adoption)
