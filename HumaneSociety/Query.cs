c3dfb76c968c47fe8a4382a8f29b2457a569cdd0
cfirgens <firgenscj@gmail.com>
2019-07-31 10:27:41 -0500

    completed the IQuery for GetShots

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -241,7 +241,8 @@ namespace HumaneSociety
         // TODO: Shots Stuff
         internal static IQueryable<AnimalShot> GetShots(Animal animal)
         {
-            throw new NotImplementedException();
+            IQueryable<AnimalShot> animalShots = db.AnimalShots.Where(a => a.AnimalId == animal.AnimalId);
+            return animalShots;
         }
 
         internal static void UpdateShot(string shotName, Animal animal)
